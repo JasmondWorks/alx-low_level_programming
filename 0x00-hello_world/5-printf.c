83ca3c283ff15f21e7e37b87e4799319e277af06
JasmondWorks <obafemilared@gmail.com>
2022-09-09 05:34:13 +0000

    Corrected the C programs (followed Betty's style)

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,8 @@
  * Return: 0
  */
 
-int main(void) {
-  printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
